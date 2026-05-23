ca7c6be37641a6ecc54329af5177eef074ccd855
Saatvik <saatvikdon@gmail.com>
2026-05-23 16:28:14 +0530

    fix: add missing CrossOrigin imports to fix compilation failure on Render

--- Backend/website/src/main/java/com/real_estate/website/controller/HealthController.java
+++ Backend/website/src/main/java/com/real_estate/website/controller/HealthController.java
@@ -1,5 +1,6 @@
 package com.real_estate.website.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
